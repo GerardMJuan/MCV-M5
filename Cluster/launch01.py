bd2b0f2739af46a3d686f7bf7dd85c224b98198d
Sergio <sergiosanchoasensio@gmail.com>
2016-02-24 19:50:16 +0100

    Now there are no errors

--- Cluster/launch01.py
+++ Cluster/launch01.py
@@ -9,8 +9,7 @@ if __name__ == '__main__':
     images += glob.glob('cars/*.bmp')
     images += glob.glob('person/*.bmp')
     images += glob.glob('none/*.bmp')
-    cmd_mask = "convert %s -limit thread 1 -interpolate spline -resize 1024x768 -sharpen
-    0x1 -normalize -resize 640x480 %s"
+    cmd_mask = "convert %s -limit thread 1 -interpolate spline -resize 1024x768 -sharpen 0x1 -normalize -resize 640x480 %s"
     print 'Launching %d tasks...' % (len(images))
     for input_filename in images:
         output_filename = input_filename[:-3] + 'png'
