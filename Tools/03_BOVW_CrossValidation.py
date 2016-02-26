466bab0d06b02a4a7d569d53534aeec535e7b3f0
Gerard Martí <gerard.marti.juan@est.fib.upc.edu>
2016-02-26 19:13:34 +0100

    HoG and LBP
    Added HOG and LBP descriptors

--- Tools/03_BOVW_CrossValidation.py
+++ Tools/03_BOVW_CrossValidation.py
@@ -1,22 +1,25 @@
 from BOVW_functions import *
 
-
-detector='SIFT'
-descriptor='SIFT'																																															
+detector='HOG'
+descriptor='HOG'
 num_samples=50000
 
+# Parameters for SIFT
 k=32
-folds=5
-start=0.01
-end=10
-numparams=30
+
+
+# Parameters for HOG
+
+
+
+# Parameters for LBP
 
 
 codebook_filename='CB_'+detector+'_'+descriptor+'_'+str(num_samples)+'samples_'+str(k)+'centroids.dat'
 visual_words_filename_train='VW_train_'+detector+'_'+descriptor+'_'+str(num_samples)+'samples_'+str(k)+'centroids.dat'
 visual_words_filename_test='VW_test_'+detector+'_'+descriptor+'_'+str(num_samples)+'samples_'+str(k)+'centroids.dat'
 
-filenames_train,GT_ids_train,GT_labels_train = prepareFiles('../../Databases/MIT_split/train/')
+filenames_train,GT_ids_train,GT_labels_train = prepareFiles('../MIT_split/train/')
 KPTS_train,DSC_train = getKeypointsDescriptors(filenames_train,detector,descriptor)
 CB=getAndSaveCodebook(DSC_train, num_samples, k, codebook_filename)
 #CB=cPickle.load(open(codebook_filename,'r'))
@@ -24,11 +27,16 @@ CB=getAndSaveCodebook(DSC_train, num_samples, k, codebook_filename)
 VW_train=getAndSaveBoVWRepresentation(DSC_train,k,CB,visual_words_filename_train)
 #VW_train=cPickle.load(open(visual_words_filename_train,'r'))
 
-
-filenames_test,GT_ids_test,GT_labels_test = prepareFiles('../../Databases/MIT_split/test/')
+filenames_test,GT_ids_test,GT_labels_test = prepareFiles('../MIT_split/test/')
 KPTS_test,DSC_test = getKeypointsDescriptors(filenames_test,detector,descriptor)
 VW_test=getAndSaveBoVWRepresentation(DSC_test,k,CB,visual_words_filename_test)
 
+
+# Do the SVM
+folds=5
+start=0.01
+end=10
+numparams=30
 ac_BOVW_L = trainAndTestLinearSVM_withfolds(VW_train,VW_test,GT_ids_train,GT_ids_test,folds,start,end,numparams)
 
 print 'Accuracy BOVW: '+str(ac_BOVW_L)
